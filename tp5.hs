abf3abf7ea16415da89325abbd08772d38d5fc8d
arnaudcoj <arnaudcoj@etudiant.univ-lille1.fr>
2015-03-22 18:27:09 +0100

    It Works !

--- tp5.hs
+++ tp5.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+{-
+Matthieu Caron
+Arnaud Cojez
+TP5 Interpretation
+-}
 --PREMIERE PARTIE
 
 import Parser
@@ -180,17 +188,18 @@ ifthenelseA = VFonctionA (\(VLitteralA (Bool b)) -> VFonctionA (\(VLitteralA (En
 
 --Q20
 
+{-
 main :: IO ()
 main = ( putStr "minilang>" >>= \_ ->
           hFlush stdout >>= \_ ->
           isEOF >>= \b ->
           if b then
-            putStr "see you later !\n"
+            putStr "\nsee you later !\n"
           else
             getLine >>= \l ->
             putStr (show (interpreteA envA (ras l)) ++ "\n") >>= \_ ->
             main)
-       --TODO
+-}
 
 --Interpreteur avec Erreurs
 
@@ -299,3 +308,139 @@ interpreteSimpleM xs (Lam nom expr) = S (VFonctionM (\v -> interpreteSimpleM ((n
 interpreteSimpleM xs (App e1 e2) = f v2
   where S (VFonctionM f) = interpreteSimpleM xs e1
         S v2 = interpreteSimpleM xs e2
+
+instance Monad SimpleM where
+    return      = S
+    (S v) >>= f = f v
+
+--Q30
+
+interpreteM :: Monad m => Environnement (ValeurM m) -> Expression -> m (ValeurM m)
+interpreteM _ (Lit l) = return (VLitteralM l)
+interpreteM xs (Var k) = return (fromJust (lookup k xs))
+interpreteM xs (Lam nom expr) = return (VFonctionM (\v -> interpreteM ((nom,v):xs) expr))
+interpreteM xs (App e1 e2) = (interpreteM xs e1 >>= \(VFonctionM f) ->
+                               interpreteM xs e2 >>= \v2 ->
+                               f v2)
+
+type InterpreteM m = Environnement (ValeurM m) -> Expression -> m (ValeurM m)
+
+interpreteS :: InterpreteM SimpleM
+interpreteS = interpreteM
+
+--Q31
+
+{-
+*Main> interpreteS [] (ras "12")
+S 12
+*Main> interpreteS [] (ras "(\\x -> x) 12")
+S 12
+*Main> interpreteS [] (ras "(\\x -> x)")
+S λ
+*Main> interpreteS [("x", (VLitteralM (Bool True)))] (ras "x")
+S True
+*Main> interpreteS [("x", (VLitteralM (Entier 3)))] (ras "x")
+S 3
+-}
+
+data TraceM v = T (Trace, v)
+              deriving Show
+
+--Q32
+
+instance Monad TraceM where
+    return v      = T ("",v)
+    T (t ,v) >>= f = T (t ++ tr, vr)
+      where T (tr,vr) = f v
+
+interpreteMT :: InterpreteM TraceM
+interpreteMT = interpreteM
+
+pingM :: ValeurM TraceM
+pingM = VFonctionM (\v -> T ("p", v))
+
+--Q33
+interpreteMT' :: InterpreteM TraceM
+interpreteMT' xs (App e1 e2) = T ((s1 ++ s2 ++ "." ++ sr), vr)
+  where T (s1, VFonctionM f) = interpreteMT' xs e1
+        T (s2, v2) = interpreteMT' xs e2
+        T (sr, vr) = f v2
+interpreteMT' xs (Lam nom expr) = T ("", VFonctionM (\v -> interpreteMT' ((nom,v):xs) expr))
+interpreteMT' xs v = interpreteMT xs v
+
+--Variante avec Erreurs
+
+data ErreurM v = Succes v
+               | Erreur String
+               deriving Show
+
+--Q34
+
+instance Monad ErreurM where
+    fail e = Erreur e
+    return = Succes
+    (Succes v) >>= f = f v
+    (Erreur m) >>= _ = fail m
+
+--Q35
+    
+interpreteE :: InterpreteM ErreurM
+interpreteE _ (Lit l) = return (VLitteralM l)
+interpreteE xs (Var v) = let j = lookup v xs
+                         in case isJust j of True -> return (fromJust j)
+                                             False -> fail ("La variable " ++ v ++ " n'est pas definie")
+interpreteE xs (Lam n e) = return (VFonctionM f)
+  where f v = interpreteE ((n,v):xs) e
+interpreteE xs (App e1 e2) =
+  (interpreteE xs e1 >>= \r ->
+    case r of
+      VFonctionM f ->
+        (interpreteE xs e2 >>= \r2 ->
+          f r2)
+      VLitteralM (Entier n) -> fail (show n ++ " n'est pas une fonction, application impossible")
+      VLitteralM (Bool b) -> fail (show b ++ " n'est pas une fonction, application impossible")
+  )
+
+--Cerise sur le gâteau
+
+class Monad m => Injectable m t where
+  injecte :: t -> ValeurM m
+
+instance Monad m => Injectable m Bool where
+  injecte = VLitteralM . Bool
+
+--Q36
+
+instance Monad m => Injectable m Integer where
+  injecte = VLitteralM . Entier
+
+--Q37
+
+instance (Monad m, Injectable m t) => Injectable m (Bool -> t) where
+    injecte f = VFonctionM (\(VLitteralM (Bool b)) -> return (injecte (f b)))
+
+instance (Monad m, Injectable m t) => Injectable m (Integer -> t) where
+    injecte f = VFonctionM (\(VLitteralM (Entier v)) -> return (injecte (f v)))
+
+--Q38
+    
+envM :: Monad m => Environnement (ValeurM m)
+envM = [ ("add",   injecte ((+) :: Integer -> Integer -> Integer))
+       , ("soust", injecte ((-) :: Integer -> Integer -> Integer))
+       , ("mult",  injecte ((*) :: Integer -> Integer -> Integer))
+       , ("quot",  injecte (quot :: Integer -> Integer -> Integer))
+       , ("et",    injecte (&&))
+       , ("ou",    injecte (||))
+       , ("non",   injecte not) ]
+
+
+main :: IO ()
+main = ( putStr "minilang>" >>= \_ ->
+          hFlush stdout >>= \_ ->
+          isEOF >>= \b ->
+          if b then
+            putStr "\nsee you later !\n"
+          else
+            getLine >>= \l ->
+            putStr (show (interpreteE envM (ras l)) ++ "\n") >>= \_ ->
+            main)
